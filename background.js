9c3b2d5cc1d67ef527644c0385e2ed24432b0b09
Dmitry Tatarinov <dmitry.tatarinov@exafore.com>
2023-11-11 16:06:56 +0200

    Add options and mirrors

--- background.js
+++ background.js
@@ -1,7 +1,24 @@
+var redirectOption = "disabled"; // По умолчанию перенаправление отключено
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === 'updateRedirectOption') {
+    redirectOption = message.option;
+  }
+});
+
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-  if (details.url.startsWith("https://www.youtube.com/watch?v=")) {
+  if (redirectOption !== "disabled" && details.url.startsWith("https://www.youtube.com/watch?v=")) {
     var videoId = details.url.split("v=")[1];
-    var redirectUrl = "https://az.piped.video/watch?v=" + videoId;
+    var redirectUrl = "https://" + redirectOption + ".piped.video/watch?v=" + videoId;
     chrome.tabs.create({ url: redirectUrl });
   }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Установите значение по умолчанию при первой установке расширения
+  chrome.storage.sync.get('redirectOption', function(data) {
+    if (!data.redirectOption) {
+      chrome.storage.sync.set({ 'redirectOption': redirectOption });
+    }
+  });
 });
