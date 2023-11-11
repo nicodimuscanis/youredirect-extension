f87cf9a050a9457f6f2dc9acda859a81db9187d5
Dmitry Tatarinov <dmitry.tatarinov@exafore.com>
2023-11-11 17:33:24 +0200

    Work with click on youtube links

--- background.js
+++ background.js
@@ -3,22 +3,47 @@ var redirectOption = "disabled"; // По умолчанию перенаправ
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === 'updateRedirectOption') {
     redirectOption = message.option;
+    chrome.storage.sync.set({ 'redirectOption': redirectOption });
   }
 });
 
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-  if (redirectOption !== "disabled" && details.url.startsWith("https://www.youtube.com/watch?v=")) {
-    var videoId = details.url.split("v=")[1];
-    var redirectUrl = "https://" + redirectOption + ".piped.video/watch?v=" + videoId;
-    chrome.tabs.create({ url: redirectUrl });
-  }
+chrome.runtime.onInstalled.addListener(function () {
+  // значение по умолчанию при первой установке расширения
+  chrome.storage.sync.get('redirectOption', function (data) {
+    redirectOption = data.redirectOption || 'disabled';
+  });
 });
 
-chrome.runtime.onInstalled.addListener(function() {
-  // Установите значение по умолчанию при первой установке расширения
+// Обработчик событий для кликов в контенте страницы
+function handleLink(url) {
   chrome.storage.sync.get('redirectOption', function(data) {
-    if (!data.redirectOption) {
-      chrome.storage.sync.set({ 'redirectOption': redirectOption });
+    redirectOption = data.redirectOption || 'disabled';
+
+    if (redirectOption !== "disabled" && url.startsWith("https://www.youtube.com/watch?v=")) {
+      var videoId = url.split("v=")[1];
+      var redirectUrl = "https://" + redirectOption + ".piped.video/watch?v=" + videoId;
+      chrome.tabs.create({ url: redirectUrl });
     }
   });
+}
+
+// обработчик для кликов
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'checkLink') {
+    handleLink(request.url);
+  }
+});
+
+// обработчик для обновления вкладок
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.url) {
+    handleLink(changeInfo.url);
+  }
+});
+
+// обработчик для события перед навигацией
+chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+  if (details.url) {
+    handleLink(details.url);
+  }
 });
--- content.js
+++ content.js
@@ -1,1 +1,9 @@
- 
+document.addEventListener('click', function (event) {
+  var target = event.target;
+
+  // Проверяем, является ли кликнутый элемент ссылкой
+  if (target.tagName === 'A') {
+    // Отправляем сообщение в background.js для проверки и, если нужно, перенаправления
+    chrome.runtime.sendMessage({ action: 'checkLink', url: target.href });
+  }
+});
